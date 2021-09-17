06abc3e726dadec76c0ad45102c5acd2058b09e2
mark-escosura <escosuramarkse@gmail.com>
2021-09-16 23:37:38 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -42,7 +42,24 @@ class Airplane {
 */
 
 class Person {
-  
+
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+
+  eat(edible){
+    if(this.stomach.length < 10) 
+      this.stomach.push(edible);
+  }
+  poop(){
+    this.stomach =[];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
+
 }
 
 /*
@@ -60,7 +77,26 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    this.tank = this.tank + gallons;
+  }
+  drive(distance) {
+    const driveMiles = this.tank * this.milesPerGallon;
+    if(distance <= driveMiles) {
+      this.odometer = distance + this.odometer;
+      this.tank = this.tank - (distance / this.milesPerGallon);
+    } else{
+      this.odometer = this.odometer + driveMiles;
+      this.tank = 0
+      return `Your vehicle is out of gas at ${this.odometer} and can't drive!`;
+    }
+  }
 }
 
 /*
