468311c5a88c393ad2573f05094d688b7484099f
mark-escosura <escosuramarkse@gmail.com>
2021-09-16 23:59:28 -0700

    Final Commit

--- index.js
+++ index.js
@@ -196,8 +196,9 @@ class Student extends Lambdasian{
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager extends Instructor {
-  constructor ({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor })
+class ProjectManager {
+  
+}
    
    
    
